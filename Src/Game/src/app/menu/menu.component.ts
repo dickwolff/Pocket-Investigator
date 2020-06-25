63abb07fd34e3433e4ece96d44a670b2e0953d62
Dick Wolff <dckwlff@gmail.com>
2020-06-25 12:02:51 +0200

    Added Temp Background, Removed E2E.

--- Src/Game/src/app/menu/menu.component.ts
+++ Src/Game/src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { version } from "../../../package.json";
+import { Router } from "@angular/router";
 
 @Component({
   templateUrl: "./menu.component.html",
@@ -11,4 +12,30 @@ export class MenuComponent {
    * The version of the game.
    */
   public gameVersion: string = version;
+
+  /**
+   * Constructor.
+   */
+  constructor(private readonly router: Router) { }
+
+  /**
+   * Navigate to Game.
+   */
+  public play(): void {
+    //this.router.navigate(["game"]); <- Doesn't exist yet.
+  }
+
+  /**
+   * Navigate to Stats.
+   */
+  public stats(): void {
+    //this.router.navigate(["stats"]); <- Doesn't exist yet.
+  }
+
+  /**
+   * Navigate to Settings.
+   */
+  public settings(): void {
+    this.router.navigate(["settings"]);
+  }
 }
