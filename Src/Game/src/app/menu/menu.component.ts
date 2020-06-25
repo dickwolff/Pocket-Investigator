5db93b3574fb7a006c99b98b39df377dced8c2a5
Dick Wolff <dckwlff@gmail.com>
2020-06-25 10:30:37 +0200

    Basic Menu Is Now Done, Bar Background Image.

--- Src/Game/src/app/menu/menu.component.ts
+++ Src/Game/src/app/menu/menu.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
+import { version } from "../../../package.json";
 
 @Component({
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
 })
-export class MenuComponent { }
+export class MenuComponent {
+
+  /**
+   * The version of the game.
+   */
+  public gameVersion: string = version;
+}
